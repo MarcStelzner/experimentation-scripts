d4c0804888089a9cd8bf1d4f1d4b64c74ab2f901
MarcStelzner <stelzner@informatik.uni-luebeck.de>
2013-04-08 21:04:42 +0300

    change of listen command to log correctly

--- src/main/assembly/scripts/wb-listen.java
+++ src/main/assembly/scripts/wb-listen.java
@@ -12,7 +12,7 @@
 	Integer protobufPort                = protobufPortString == null ? null : Integer.parseInt(protobufPortString);
 	boolean useProtobuf                 = protobufHost != null && protobufPort != null;
 
-	SessionManagement sessionManagement = WisebedServiceHelper.getSessionManagementService(sessionManagementEndpointURL);
+	SessionManagement sessionManagement = WSNServiceHelper.getSessionManagementService(sessionManagementEndpointURL);
 
 //--------------------------------------------------------------------------
 // Application logic
@@ -23,21 +23,26 @@
 			for (int i=0; i<msgs.size(); i++) {
 				Message msg = (Message) msgs.get(i);
 				synchronized(System.out) {
-					
-					String text = StringUtils.replaceNonPrintableAsciiCharacters(new String(msg.getBinaryData()));
+					String msgString = StringUtils.toHexString(msg.getBinaryData());
+					if(msgString.startsWith("0x6c")){	
+						//a log needs to be formatted and printed
+						System.out.println(unhex(msg));
+					} else {
+						String text = StringUtils.replaceNonPrintableAsciiCharacters(new String(msg.getBinaryData()));
 
-					if (csv) {
-						text = text.replaceAll(";", "\\;");
+						if (csv) {
+							text = text.replaceAll(";", "\\;");
+						}
+
+						System.out.print(new org.joda.time.DateTime(msg.getTimestamp().toGregorianCalendar()));
+						System.out.print(csv ? ";" : " | ");
+						System.out.print(msg.getSourceNodeId());
+						System.out.print(csv ? ";" : " | ");
+						System.out.print(text);
+						System.out.print(csv ? ";" : " | ");
+						System.out.print(msgString);
+						System.out.println();
 					}
-
-					System.out.print(new org.joda.time.DateTime(msg.getTimestamp().toGregorianCalendar()));
-					System.out.print(csv ? ";" : " | ");
-					System.out.print(msg.getSourceNodeId());
-					System.out.print(csv ? ";" : " | ");
-					System.out.print(text);
-					System.out.print(csv ? ";" : " | ");
-					System.out.print(StringUtils.toHexString(msg.getBinaryData()));
-					System.out.println();
             	}
 			}
 		}
@@ -57,6 +62,60 @@
 		public void experimentEnded() {
 			System.err.println("Experiment ended");
 			System.exit(0);
+		}
+		private String unhex(Message msg){
+			resultString = "";
+			String msgString = StringUtils.toHexString(msg.getBinaryData());
+			//is it a logmessage ? 108(int8) = 6c(hex) = l(ASCII)
+			try{
+				//now include node-address (only last 4 chars needed)
+				resultString = msg.getSourceNodeId().substring(msg.getSourceNodeId().length()-4,msg.getSourceNodeId().length()) + ";";
+				//include time
+				String microseconds = "0000";
+				long milliseconds = msg.getTimestamp().toGregorianCalendar().getTimeInMillis();
+				long seconds = milliseconds/1000;
+				milliseconds = milliseconds % 1000;
+				String secondsString = Long.toHexString(seconds);
+				String millisecondsString = Long.toHexString(milliseconds);
+				while(secondsString.length() < 8){
+					secondsString = "0"+secondsString;
+				}
+				while(millisecondsString.length() < 4){
+					millisecondsString = "0"+millisecondsString;
+				}
+				//get it to 
+				resultString += secondsString + ";" + millisecondsString + ";" + microseconds + ";";
+				//Integer.toHexString(seconds)
+				//include the rest of the nodes information
+				//elide the starting element
+				msgString = msgString.substring(4, msgString.length()) + " ";
+				//elide all hex-signs, but add zeros //TODO: Do this more efficently
+				msgString = msgString.replaceAll("x0 ","x00 ");
+				msgString = msgString.replaceAll("x1 ","x01 ");
+				msgString = msgString.replaceAll("x2 ","x02 ");
+				msgString = msgString.replaceAll("x3 ","x03 ");
+				msgString = msgString.replaceAll("x4 ","x04 ");
+				msgString = msgString.replaceAll("x5 ","x05 ");
+				msgString = msgString.replaceAll("x6 ","x06 ");
+				msgString = msgString.replaceAll("x7 ","x07 ");
+				msgString = msgString.replaceAll("x8 ","x08 ");
+				msgString = msgString.replaceAll("x9 ","x09 ");
+				msgString = msgString.replaceAll("xa ","x0a ");
+				msgString = msgString.replaceAll("xb ","x0b ");
+				msgString = msgString.replaceAll("xc ","x0c ");
+				msgString = msgString.replaceAll("xd ","x0d ");
+				msgString = msgString.replaceAll("xe ","x0e ");
+				msgString = msgString.replaceAll("xf ","x0f ");
+				msgString = msgString.replaceAll(" 0x","");
+				//include seperation signs after element 3(source),7(destination),11(link_metric) and 15(len)
+				// (couting from 0)
+				msgString = msgString.substring(0,4)+";"+msgString.substring(4,8)+";"+msgString.substring(8,12)+";"+msgString.substring(12,16)+";"+msgString.substring(16,msgString.length());
+				resultString += msgString;
+				return resultString;
+			}
+			catch(Exception e){
+				return "BAD FORMAT FOR LOGFILE!";
+			}
 		}
 	};
 
@@ -101,7 +160,7 @@
 	}
 
 	log.debug("Got a WSN instance URL, endpoint is: {}", wsnEndpointURL);
-	WSN wsnService = WisebedServiceHelper.getWSNService(wsnEndpointURL);
+	WSN wsnService = WSNServiceHelper.getWSNService(wsnEndpointURL);
 	final WSNAsyncWrapper wsn = WSNAsyncWrapper.of(wsnService);
 
 	while(true) {
